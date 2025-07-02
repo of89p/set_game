5480252d6177ac139834fc9e35518f9f49c7506a
Terence Teo <terenceteoly@gmail.com>
2025-07-02 23:43:01 +0800

    Created view & view model files

---
+++ set_game/SetGame_Model.swift
@@ -0,0 +1,8 @@
+//
+//  SetGame_Model.swift
+//  set_game
+//
+//  Created by Terence Teo on 2/7/25.
+//
+
+import Foundation
---
+++ set_game/SetGame_ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SetGame_ViewModel.swift
+//  set_game
+//
+//  Created by Terence Teo on 2/7/25.
+//
+
+import Foundation
