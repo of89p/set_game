aba89f8ab9b9507355b3d21343c931dbed5557d6
Terence <terenceteoly@gmail.com>
2025-07-02 23:03:57 +0800

    Initial Commit

---
+++ set_game/set_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  set_gameApp.swift
+//  set_game
+//
+//  Created by Terence Teo on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct set_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ set_gameTests/set_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  set_gameTests.swift
+//  set_gameTests
+//
+//  Created by Terence Teo on 2/7/25.
+//
+
+import Testing
+@testable import set_game
+
+struct set_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
