f9ce54933b4f517306dac81dad3bf0bc51f39276
Terence Teo <terenceteoly@gmail.com>
2025-07-03 09:48:01 +0800

    Established connection between model and viewModel

--- set_game/SetGame_ViewModel.swift
+++ set_game/SetGame_ViewModel.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 
-struct cardCategory<categoryType> {
-    var typeOne: categoryType
-    var typeTwo: categoryType
-    var typeThree: categoryType
+class SetGame_ViewModel: ObservableObject {
+    struct cardCategory<categoryType> {
+        var typeOne: categoryType
+        var typeTwo: categoryType
+        var typeThree: categoryType
+    }
+
+    var typeOfCards: [String: Any] = [
+        "number": cardCategory<Int>(typeOne: 1, typeTwo: 2, typeThree: 3),
+        "shape": cardCategory<String>(typeOne: "diamond", typeTwo: "squiggle", typeThree: "oval"),
+        "shading": cardCategory<String>(typeOne: "solid", typeTwo: "stripped", typeThree: "open"),
+        "color": cardCategory<Color>(typeOne: Color.red, typeTwo: Color.green, typeThree: Color.purple)
+    ]
+    
+    @Published private var model = SetGame_Model()
 }
-
-var typeOfCards: [String: Any] = [
-    "number": cardCategory<Int>(typeOne: 1, typeTwo: 2, typeThree: 3),
-    "shape": cardCategory<String>(typeOne: "diamond", typeTwo: "squiggle", typeThree: "oval"),
-    "shading": cardCategory<String>(typeOne: "solid", typeTwo: "stripped", typeThree: "open"),
-    "color": cardCategory<Color>(typeOne: Color.red, typeTwo: Color.green, typeThree: Color.purple)
-]
-
