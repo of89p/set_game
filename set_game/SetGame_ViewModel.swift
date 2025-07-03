91b6cf281f860ae1c3b807f8f6f7d8f54031bf8e
Terence Teo <terenceteoly@gmail.com>
2025-07-03 17:25:26 +0800

    fixed init issue

--- set_game/SetGame_ViewModel.swift
+++ set_game/SetGame_ViewModel.swift
@@ -20,48 +20,49 @@ class SetGame_ViewModel: ObservableObject {
     private(set) var allCardContents: [CardContent]
     
     init(){
-        allCardContents = []
-        
-        let typeOfCards = [
-            "number": ["1","2","3"],
-            "shape": ["diamond", "squiggle", "oval"],
-            "shading": ["solid", "stripped", "open"],
-            "color": ["red", "green", "purple"]
-        ]
-        
-        func generateCardContent(){
-            if let numbers = typeOfCards["number"],
-               let shapes = typeOfCards["shape"],
-               let shadings = typeOfCards["shading"],
-               let colors = typeOfCards["color"]
-            {
-                for number in numbers {
-                    for shape in shapes {
-                        for shading in shadings {
-                            for color in colors {
-                                allCardContents.append(CardContent(number: number, shape: shape, shading: shading, color: color))
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-
-
+        allCardContents = SetGame_ViewModel.generateCardContent()
         model = SetGame_ViewModel.createSetGame(numberOfCards: allCardContents.count, allCardContents: allCardContents)
-        
-        
-        generateCardContent()
-        
-        print("allCardContents \(allCardContents)")
     }
     
+
     private static func createSetGame(numberOfCards: Int, allCardContents: [CardContent]) -> SetGame_Model<CardContent> {
         return SetGame_Model<CardContent>(numberOfCards: allCardContents.count){ index in
             return allCardContents[index]
         }
     }
+    
+    static let typeOfCards = [
+        "number": ["1","2","3"],
+        "shape": ["diamond", "squiggle", "oval"],
+        "shading": ["solid", "stripped", "open"],
+        "color": ["red", "green", "purple"]
+    ]
+    
+    static func generateCardContent() -> [CardContent]{
+        var returnArr: [CardContent] = []
         
+        if let numbers = typeOfCards["number"],
+           let shapes = typeOfCards["shape"],
+           let shadings = typeOfCards["shading"],
+           let colors = typeOfCards["color"]
+        {
+        for number in numbers {
+            for shape in shapes {
+                for shading in shadings {
+                    for color in colors {
+                        returnArr.append(CardContent(number: number, shape: shape, shading: shading, color: color))
+                    }
+                }
+            }
+            }
+        }
+        
+        return returnArr
+    }
+    
+    var cards: Array<SetGame_Model<CardContent>.Card> {
+        print(model.cards)
+        return model.cards
+    }
    
 }
